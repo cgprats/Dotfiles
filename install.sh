850a3129c4805bdaa685c6fdbf3947e2289f8609
Christopher Prats <christopherprats@gmail.com>
2022-12-24 17:08:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,8 +19,6 @@ echo "Installing tmux plugin manager"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Note: Please press C-b and then i to install tpm plugins"
 
-echo "Installing vim-plug"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Please run :PlugInstall in Normal mode to activate neovim plugins"
 echo "Please install the coc-extensions you wish to use"
 echo "Please see: https://github.com/neoclide/coc.nvim/wiki/Using-coc-extensions for a list"
