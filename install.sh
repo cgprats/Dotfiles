ffd9b073b5083054f69cf325052b50740ddfc722
Christopher Prats <christopherprats@gmail.com>
2023-01-13 21:09:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,25 +8,25 @@ then
     echo "Detected Linux distribution: $ID"
     case "$ID" in
     fedora*)
-      sudo dnf install zsh vim-enhanced emacs tmux neovim neofetch nodejs npm mosh
+      sudo dnf install zsh vim-enhanced emacs tmux neovim neofetch mosh
       ;;
     ubuntu*)
-      sudo apt install zsh vim emacs tmux neovim neofetch nodejs npm mosh
+      sudo apt install zsh vim emacs tmux neovim neofetch mosh
       ;;
     opensuse*)
-      sudo zypper install zsh vim emacs tmux neovim nodejs-default npm-default mosh
+      sudo zypper install zsh vim emacs tmux neovim mosh
       ;;
     debian*)
-      sudo apt install vim emacs tmux neovim neofetch nodejs npm mosh
+      sudo apt install vim emacs tmux neovim neofetch mosh
       ;;
      *)
-      pkcon install zsh vim-enhanced emacs tmux neovim neofetch nodejs npm mosh
+      pkcon install zsh vim-enhanced emacs tmux neovim neofetch mosh
       ;;
     esac
   fi
 elif [[ "$OSTYPE" == "darwin"* ]]
 then
-  brew install zsh vim emacs tmux neovim neofetch node npm mosh
+  brew install zsh vim emacs tmux neovim neofetch mosh
 fi
 
 echo "Setting zsh as default shell"
@@ -36,9 +36,6 @@ echo "Note: Please install a Nerd Font of your choice to use powerlevel10k"
 echo "Please see: https://github.com/ryanoasis/nerd-fonts"
 
 echo "Note: Please press C-b and then i to install tpm plugins"
-
-echo "Please install the coc-extensions you wish to use"
-echo "Please see: https://github.com/neoclide/coc.nvim/wiki/Using-coc-extensions for a list"
 
 echo "Copying config files"
 cp -v .zshrc ~/.zshrc
